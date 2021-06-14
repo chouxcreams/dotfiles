c8392abb0671460f30a89ee038a37ffbaf69c10b
Shutarou Takayama <hi.mountain110916@gmail.com>
2021-06-15 00:43:25 +0900

    lint errorの修正

--- init.sh
+++ init.sh
@@ -1,15 +1,15 @@
 #! /bin/bash -e
 
 DOTPATH="$HOME/dotfiles"
-ln -s $DOTPATH/.zshenv $HOME/.zshenv
-ln -s $DOTPATH/.nanorc $HOME/.nanorc
+ln -s "$DOTPATH"/.zshenv "$HOME"/.zshenv
+ln -s "$DOTPATH"/.nanorc "$HOME"/.nanorc
 
 # gitの補完用スクリプトのダウンロード
-curl -o $HOME/dotfiles/.zsh/completion/git-completion.bash https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash
-curl -o $HOME/dotfiles/.zsh/completion/_git https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.zsh
+curl -o "$HOME"/dotfiles/.zsh/completion/git-completion.bash https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash
+curl -o "$HOME"/dotfiles/.zsh/completion/_git https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.zsh
 
 # dockerの補完用スクリプトのダウンロード
-curl -o $HOME/dotfiles/.zsh/completion/_docker https://raw.githubusercontent.com/docker/cli/master/contrib/completion/zsh/_docker
+curl -o "$HOME"/dotfiles/.zsh/completion/_docker https://raw.githubusercontent.com/docker/cli/master/contrib/completion/zsh/_docker
 
 # nanoのシンタックスハイライトをインストール
-git clone https://github.com/scopatz/nanorc.git $HOME/dotfiles/.nano
+git clone https://github.com/scopatz/nanorc.git "$HOME"/dotfiles/.nano
