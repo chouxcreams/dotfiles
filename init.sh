3d499819ae37fbd466ca14bcf548112d48a60b31
Shutarou Takayama <hi.mountain110916@gmail.com>
2021-06-14 23:55:42 +0900

    nanoの設定を追加

--- init.sh
+++ init.sh
@@ -2,6 +2,7 @@
 
 DOTPATH="$HOME/dotfiles"
 ln -s $DOTPATH/.zshenv $HOME/.zshenv
+ln -s $DOTPATH/.nanorc $HOME/.nanorc
 
 # gitの補完用スクリプトのダウンロード
 curl -o $HOME/dotfiles/.zsh/completion/git-completion.bash https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash
@@ -9,3 +10,6 @@ curl -o $HOME/dotfiles/.zsh/completion/_git https://raw.githubusercontent.com/gi
 
 # dockerの補完用スクリプトのダウンロード
 curl -o $HOME/dotfiles/.zsh/completion/_docker https://raw.githubusercontent.com/docker/cli/master/contrib/completion/zsh/_docker
+
+# nanoのシンタックスハイライトをインストール
+git clone https://github.com/scopatz/nanorc.git $HOME/dotfiles/.nano
