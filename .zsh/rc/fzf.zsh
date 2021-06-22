64a236dc7cef4f076026f823199c557f3555227f
Shutarou Takayama <hi.mountain110916@gmail.com>
2021-06-23 02:33:49 +0900

    fzf 設定追加

---
+++ .zsh/rc/fzf.zsh
@@ -0,0 +1,6 @@
+gsw () {
+    local branches branch
+    branches=$(git --no-pager branch -vv) &&
+    branch=$(echo "$branches" | fzf +m) &&
+    git switch $(echo "$branch" | awk '{print $1}' | sed "s/.* //")
+}
