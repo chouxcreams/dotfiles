a3dbd487750195e2136190a705b837460eab4829
Shutarou Takayama <hi.mountain110916@gmail.com>
2021-09-08 13:11:15 +0900

    remote branch

--- .zsh/rc/fzf.zsh
+++ .zsh/rc/fzf.zsh
@@ -10,3 +10,12 @@ fcd() {
     dir=$(find ${1:-.} -path '*/\.*' -prune -o -type d -print 2> /dev/null | fzf +m) &&
     cd "$dir"
 }
+
+# fbr - checkout git branch (including remote branches), sorted by most recent commit, limit 30 last branches
+gsfr() {
+    local branches branch
+    branches=$(git for-each-ref --count=30 --sort=-committerdate refs/heads/ --format="%(refname:short)") &&
+    branch=$(echo "$branches" |
+            fzf-tmux -d $(( 2 + $(wc -l <<< "$branches") )) +m) &&
+    git checkout $(echo "$branch" | sed "s/.* //" | sed "s#remotes/[^/]*/##")
+}
