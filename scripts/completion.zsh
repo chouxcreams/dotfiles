745e23e117206d38b9ed19d57d2e094465cd88e2
Shutarou Takayama <40602023+chouxcreams@users.noreply.github.com>
2024-04-22 21:48:40 +0900

    refactor: 不要な補完の削除とdenoの保管の追加

--- scripts/completion.zsh
+++ scripts/completion.zsh
@@ -1,13 +1,5 @@
 #!/usr/bin/env zsh
-if (( ${+commands[gibo]} )); then
-    gibo completion zsh > "$XDG_DATA_HOME"/zsh/completions/_gibo
-fi
-
-if (( ${+commands[volta]} )); then
-    volta completions zsh > "$XDG_DATA_HOME"/zsh/completions/_volta
-fi
-
-if (( ${+commands[sheldon]} )); then
-    sheldon completions --shell zsh > "$XDG_DATA_HOME"/zsh/completions/_sheldon
+if (( ${+commands[deno]} )); then
+    deno completions zsh > "$XDG_DATA_HOME"/zsh/completions/_deno
 fi
 
