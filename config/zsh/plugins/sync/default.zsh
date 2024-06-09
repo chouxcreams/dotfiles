8a82b25e4a58a1f2db26868dcad9d789149a284f
Shutarou Takayama <40602023+chouxcreams@users.noreply.github.com>
2024-06-09 23:32:38 +0900

    feat: iterm2のタブにカレントディレクトリを表示

--- config/zsh/plugins/sync/default.zsh
+++ config/zsh/plugins/sync/default.zsh
@@ -3,3 +3,8 @@
 eval "$(starship init zsh)"
 # eval "$(gh completion -s zsh)"
 eval "$(direnv hook zsh)"
+### ディレクトリ移動するたびに呼び出される関数
+function chpwd() {
+    echo -ne "\033]0;$(pwd | sed 's|.*/||')\007"
+}
+autoload -Uz chpwd && chpwd
