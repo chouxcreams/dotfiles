2866fe0fca62c0c68fb1f50110361566765c57d9
Shutarou Takayama <40602023+chouxcreams@users.noreply.github.com>
2025-01-11 17:25:24 +0900

    feat: ryeの設定を追加

--- config/zsh/plugins/defer/default.zsh
+++ config/zsh/plugins/defer/default.zsh
@@ -118,6 +118,9 @@ export BUNDLE_USER_CONFIG="$XDG_CONFIG_HOME"/bundle
 export BUNDLE_USER_CACHE="$XDG_CACHE_HOME"/bundle
 export BUNDLE_USER_PLUGIN="$XDG_DATA_HOME"/bundle
 
+### Rye ###
+export RYE_HOME="$XDG_DATA_HOME"/rye
+
 ### sbt ###
 export SBT_OPTS="-ivy "$XDG_DATA_HOME"/ivy2 -sbt-dir "$XDG_DATA_HOME"/sbt"
 
@@ -169,6 +172,7 @@ path=(
     $VOLTA_HOME/bin(N-/)
     $POETRY_HOME/bin(N-/)
     $PIPX_BIN_DIR(N-/)
+    $RYE_HOME/shims/(N-/)
     $path
 )
 
