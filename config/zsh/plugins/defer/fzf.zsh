5222f350b1c05e67c0584f0621af4044e30a7e39
Shutarou Takayama <40602023+chouxcreams@users.noreply.github.com>
2025-06-24 21:51:41 +0900

    feat: ghq移動をzenoプラグインに変更

--- config/zsh/plugins/defer/fzf.zsh
+++ config/zsh/plugins/defer/fzf.zsh
@@ -57,10 +57,3 @@ zj() {
   zellij attach "$session"
 }
 
-function gg() {
-    FZF_DEFAULT_OPTS="${FZF_DEFAULT_OPTS} --reverse --height=50%"
-    local root="$(ghq root)"
-    local repo="$(ghq list | fzf --preview="glow ${root}/{1}/README.md")"
-    local dir="${root}/${repo}"
-    [ -n "${dir}" ] && cd "${dir}"
-}
